9f7ce740b4bf96a2b7a550713b8f1faa9b9dcb39
Samuel SIlva <samuelsilvawb@gmail.com>
2021-10-06 21:43:35 +0100

    feat: Include limits on screen

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 async function enableGetUserMedia() {
-  const player = document.getElementById("video");
+  const video = document.getElementById("video");
   const stream = await navigator.mediaDevices.getUserMedia({
-    video: true,
+    video: {
+      facingMode: "environment",
+      width: window.innerWidth
+    }
   });
-  player.srcObject = stream;
+  video.srcObject = stream;
 }
 
-(async () => {
+async function enableDeviceOrientation() {
   // --- Starting to get device metadata ---- //
-  // NOTE: This validation is for SAFARI
   if (
     DeviceOrientationEvent &&
     typeof DeviceOrientationEvent.requestPermission === "function"
@@ -18,16 +20,70 @@ async function enableGetUserMedia() {
     if (permission === "granted") {
       function handleOrientation(event) {
         const absolute = event.absolute;
+
+        // Z axis
         const alpha = event.alpha;
+
+        // X axis
+        // relative to the plane of the screen
+        // positive towards the right
+        // negative towards the left
         const beta = event.beta;
+
+        // Y axis
         const gamma = event.gamma;
 
-        const orientationDataElement =
-          document.getElementById("orientation-data");
+        const orientationDataElement = document.getElementById(
+          "orientation-data"
+        );
+        const orientationElement = document.getElementById("orientation");
         orientationDataElement.innerHTML = `Absolute: ${absolute}, Alpha: ${alpha}, Beta: ${beta}, Gamma: ${gamma}`;
+        orientationElement.innerHTML = `${
+          window.screen && window.screen.orientation
+            ? window.screen.orientation.type
+            : window.orientation
+        }`;
       }
 
       window.addEventListener("deviceorientation", handleOrientation);
     }
   }
-})();
+}
+
+function drawCanvas({ canvas, width, height, img }) {
+  canvas.width = width;
+  canvas.height = height;
+
+  const ratio = Math.min(canvas.width / img.width, canvas.height / img.height);
+  const x = (canvas.width - img.width * ratio) / 2;
+  const y = (canvas.height - img.height * ratio) / 2;
+
+  canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+  canvas
+    .getContext("2d")
+    .drawImage(
+      img,
+      0,
+      0,
+      img.width,
+      img.height,
+      x,
+      y,
+      img.width * ratio,
+      img.height * ratio
+    );
+}
+
+function takePicture() {}
+
+document
+  .getElementById("user-media-button")
+  .addEventListener("click", enableGetUserMedia);
+
+document
+  .getElementById("device-orientation-button")
+  .addEventListener("click", enableDeviceOrientation);
+
+document
+  .getElementById("take-picture-button")
+  .addEventListener("click", takePicture);
